cd94115ba36806b3b53e04b374f6d30816e6c84a
fnabulsi <33299094+fnabulsi@users.noreply.github.com>
2017-11-12 13:09:38 -0800

    Update sicxe_asm.h

--- sicxe_asm.h
+++ sicxe_asm.h
@@ -8,6 +8,8 @@
 #include "opcodetab.h"
 #include "opcode_error_exception.h"
 #include "symtab.h"
+#include "symtab_exception.h"
+
 
 using namespace std;
 
@@ -21,6 +23,7 @@ private:
     struct list {
         int line;
         int locctr;
+        int machine_code;
         string label;
         string opcode;
         string operand;
@@ -29,12 +32,36 @@ private:
     };
     int loc_ctr;        //global location counter that will be updates as the file is parsed.
     vector<list> storage;
+    string base;
     int directive_handler(int directive, string operand, int currentlcctr, int line);
     void is_operand_digit(string operand, int line);
     void is_operand_alnum(string operand, int line);
-    int find_start(file_parser, opcodetab, symtab);
-    void assign_addresses(int line, file_parser fp, opcodetab opcodes, symtab labels);
-
-
-    };
-
+    int find_start();
+    void assign_addresses(int line);
+    int do_first_pass(string file);
+    void do_second_pass(string file, int line);
+    void print_list_file(string file);
+    bool is_assembler_directive(string opcode, int line);
+    void process_assembler_directive(string opcode, string operand, int line);
+    void process_BYTE(string operand, int line);
+    void process_WORD(string operand, int line);
+    void process_BASE(string operand, int line);
+    void process_NOBASE(string operand, int line);
+    void process_END(string operand, int line);
+    void process_line(string opcode, string operand, int line);
+    void process_format1(string opcode, int line);
+    void process_format2(string opcode, string operand, int line);
+    void process_format3(string opcode, string operand, int line);
+    void process_format4(string opcode, string operand, int line);
+    string string_to_ascii(string s);
+    int hex_to_int(string s);
+    bool is_number(string s);
+    int string_to_int(string s);
+    void get_offset(int &code, string operand, int line);
+    void process_operand(int &code, string opcode, string operand, int line);
+    
+    file_parser *fp;
+    opcodetab *opcodes;
+    symtab *labels;
+    ostringstream convert_to_string;
+};
